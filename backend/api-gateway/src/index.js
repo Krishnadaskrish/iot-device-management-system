418236ae850553faf1d624cab7a3d543deec97b6
hornto <krishnadas10.official@gmail.com>
2024-06-27 17:55:48 +0530

    make build in to api-gate way and user-service

---
+++ backend/api-gateway/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const port = process.env.PORT || 3000
+
+
+
+// Proxy for User Service
+app.use('/api/user', createProxyMiddleware({
+    target: process.env.USER_SERVICE_URL,
+    changeOrigin: true
+  }));
+
+
+
+app.listen(port,()=>{
+    console.log(`api gate way is running on ${port}`)
+})
--- backend/user-service/src/index.js
+++ backend/user-service/src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config()
+
 
 const userRoute = require('./adapters/express/routes/userRouter')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 
-dotenv.config()
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://krishnadas10official:fFs00SzLQF63TU4D@cluster0.okzakwg.mongodb.net/CrudTask";
 
 app.use(express.json())
 
 
-app.use('/api',userRoute)
+app.use('/api/user',userRoute)
 
 mongoose.connect(MONGODB_URI, )
   .then(() => {
